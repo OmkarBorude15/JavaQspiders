befeb1741acd6f901515523635d623c6b911fff5
Omkar Borude <155351317+OmkarBorude15@users.noreply.github.com>
2025-02-07 11:10:16 +0530

    Add files via upload

---
+++ EvenOddWithoutModulus.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class EvenOddWithoutModulus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int num = sc.nextInt();
+    
+        System.out.println(num/2 == num/2.0);
+        System.out.println((num/2)*2 == num);
+    }  
+}
